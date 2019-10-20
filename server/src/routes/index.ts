55a7c974a98178a8740b96af479fccb4e00d3ad9
Sapir Malka <sapirm290@gmail.com>
2019-10-20 23:06:06 +0300

    serving the client in '/' route

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,10 +1,10 @@
 import { NextFunction, Request, Response } from "express";
 var express = require('express');
+var path = require('path');
 var router = express.Router();
-
 /* GET home page. */
 router.get('/', function (req: Request, res: Response, next: NextFunction) {
-  res.render('index', { title: 'Express' });
+  res.sendFile(path.join(__dirname, '../../../client/build/index.html'));
 });
 
 module.exports = router;
